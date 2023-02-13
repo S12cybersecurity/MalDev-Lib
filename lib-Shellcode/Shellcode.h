253911d21572f9676f8c66e3a91c7e7628b3d4a8
S12cybersecurity <79543461+S12cybersecurity@users.noreply.github.com>
2023-02-13 22:14:16 +0100

    Add files via upload

--- lib-Shellcode/Shellcode.h
+++ lib-Shellcode/Shellcode.h
@@ -27,13 +27,13 @@ public:
         return len;
     }
 
-    void XOR_decrypt(unsigned char key) {
+    unsigned char * XOR_decrypt(unsigned char key) {
         for (int i = 0; i < len; i++) {
             payload[i] = payload[i] ^ key;
-            cout << "\\x" << hex << setfill('0') << setw(2) << (int)payload[i];
         }
-        cout << endl;
-        }
+        return payload;
+    }
+
 
     void XOR_encrypt(unsigned char key){
         for(int i = 0; i < len; i++){
